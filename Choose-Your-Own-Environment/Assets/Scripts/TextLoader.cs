2227590bdc2cadbe9a2056da762a1e68d9de2668
pacificyost <pacificyost@gmail.com>
2018-04-14 01:34:52 -0700

    Some basic test code
    Text reading and writing to a UI text field.  A basic scriptable object
    assuming no yaml scripting.

---
+++ Choose-Your-Own-Environment/Assets/Scripts/TextLoader.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextLoader : MonoBehaviour {
+    public TextAsset textFile;
+	// Use this for initialization
+	void Start () {
+        Text text = FindObjectOfType<Text>();
+        text.text = textFile.text;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
