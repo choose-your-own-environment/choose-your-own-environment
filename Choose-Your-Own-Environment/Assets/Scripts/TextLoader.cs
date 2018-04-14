3ad63ee7daf0f956dda9286b943f28cd0652ba8c
pacificyost <pacificyost@gmail.com>
2018-04-14 12:03:01 -0700

    Created text parser
    Created text parser.  It will take one text file per scene.

--- Choose-Your-Own-Environment/Assets/Scripts/TextLoader.cs
+++ Choose-Your-Own-Environment/Assets/Scripts/TextLoader.cs
@@ -1,18 +1,81 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class TextLoader : MonoBehaviour {
-    public TextAsset textFile;
+    public TextAsset SourceText;
 	// Use this for initialization
 	void Start () {
-        Text text = FindObjectOfType<Text>();
-        text.text = textFile.text;
+
+        dictionary = new Dictionary<string, List<string>>();
+
+        ParseText(SourceText.text);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    private void ParseText(string input)
+    {
+        using (StringReader sr = new StringReader(input))
+        {
+            string line = string.Empty;
+            int delimLocation = -1;
+            string currentKey = string.Empty;
+            string currentVal = string.Empty;
+            do
+            {
+                line = sr.ReadLine();
+
+                if (line != null && line.Contains(delimiter))
+                {
+                    if (currentKey != string.Empty)
+                    {
+                        AddValue(currentKey, currentVal);
+                        Debug.Log(currentKey);
+                        Debug.Log(currentVal);
+
+                        currentKey = string.Empty;
+                        currentVal = string.Empty;
+                    }
+                    
+                    delimLocation = line.IndexOf(delimiter);
+
+                    currentKey = line.Substring(0, delimLocation);
+                    currentVal = line.Substring(delimLocation + delimiter.Length);
+
+                    
+                }
+                else
+                {
+                    currentVal = string.Concat(currentVal, Environment.NewLine, line);
+                }
+
+            } while (line != null);
+            AddValue(currentKey, currentVal);
+            Debug.Log(currentKey);
+            Debug.Log(currentVal);
+        }
+
+        
+    }
+
+    private void AddValue(string key, string val)
+    {
+        if (dictionary.ContainsKey(key))
+        {
+            dictionary[key].Add(val);
+        }
+        else
+        {
+            List<string> newList = new List<string>();
+            newList.Add(val);
+            dictionary.Add(key, newList);
+        }
+    }
+
+
+    Dictionary<string, List<string>> dictionary;
+
+    string delimiter = ">:";
 }
