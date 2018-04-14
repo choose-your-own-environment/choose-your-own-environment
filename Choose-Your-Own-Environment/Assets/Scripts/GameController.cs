6c977c96cf214f27262032826099ed2b84bf6a72
pacificyost <pacificyost@gmail.com>
2018-04-14 13:50:20 -0700

    Adding Scenes to build profile
    Adding scenes to build profile. Adding template audio manager prefab.
    Adding template game controller, Adding static scene controller class to
    simplify scene transitions.

---
+++ Choose-Your-Own-Environment/Assets/Scripts/GameController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private const int titleIndex = 0;
+    private const int prologueIndex = 1;
+    private const int conversationIndex = 2;
+    private const int epilogueIndex = 3;
+    private const int endIndex = 4;
+}
---
+++ Choose-Your-Own-Environment/Assets/Scripts/SceneController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class SceneController {
+    public static void LoadLevel(int levelID)
+    {
+        SceneManager.LoadScene(levelID);
+    }
+
+    public static void LoadTitle()
+    {
+        SceneManager.LoadScene("Title");
+    }
+
+    public static void LoadEndScene()
+    {
+        SceneManager.LoadScene("End");
+    }
+
+    public static void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
